05875460a20cc4dd942631f8a3445228c9cfe3a8
Bill <billrdunn@gmail.com>
2022-01-21 08:49:12 +0000

    Complete exercise 5.7

--- src/components/Blog.js
+++ src/components/Blog.js
@@ -1,8 +1,50 @@
-import React from 'react'
-const Blog = ({ blog }) => (
-  <div>
-    {blog.title} {blog.author}
-  </div>
-)
+import React, { useState } from 'react'
+
+const Blog = ({ blog }) => {
+
+  const blogStyle = {
+    paddingTop: 10,
+    paddingLeft: 2,
+    border: 'solid',
+    borderWidth: 1,
+    marginBottom: 5
+  }
+
+  const [showFull, setShowFull] = useState(false)
+
+  const handleClick = () => {
+    setShowFull(!showFull)
+  }
+
+  const showFullInfo = () => (
+    <div>
+      {blog.title}
+      <br></br>
+      {blog.author}
+      <br></br>
+      {blog.url}
+      <br></br>
+      likes: {blog.likes}
+      <button>like</button>
+      <br></br>
+      <button onClick={handleClick}> hide </button>
+    </div>
+  )
+
+  const hideFullInfo = () =>
+    (
+      <div>
+        {blog.title} by {blog.author}
+        <br></br>
+        <button onClick={handleClick}> show info </button>
+      </div>
+    )
+
+  return (
+    <div style={blogStyle}>
+      {showFull ? showFullInfo () : hideFullInfo()}
+    </div>
+  )
+}
 
 export default Blog
